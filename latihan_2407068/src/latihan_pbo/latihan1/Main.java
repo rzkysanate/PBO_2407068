17a9b81c907f7a025e1b641f929e5a533ff70655
rzkysanate <rizkypermana0501@gmail.com>
2026-02-04 09:38:53 +0700

    latihan class dan atribute

--- latihan_2407068/src/latihan_pbo/latihan1/Main.java
+++ latihan_2407068/src/latihan_pbo/latihan1/Main.java
@@ -1,7 +1,21 @@
 package latihan_pbo.latihan1;
 
+class Cafe{
+    String nama;
+    String alamat;
+
+    Cafe(String nama, String alamat){
+        this.nama = nama;
+        this.alamat = alamat;
+        System.out.println(nama);
+        System.out.println(alamat);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Praktikum Pertama");
+
+        Cafe cafe1 = new Cafe("Kopi Kenangan", "Indramayu");
+        Cafe cafe2 = new Cafe("Tomoro Coffe", "Cirebon");
     }
 }
